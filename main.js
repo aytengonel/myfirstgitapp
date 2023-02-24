aa2da298a2e31ac2877b34cd2c359d553cdc071b
aytengonel <79304202+aytengonel@users.noreply.github.com>
2023-02-24 11:49:18 +0100

    first
    first

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const video = document.getElementById("video");
 
-setupCounter(document.querySelector('#counter'))
+navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
+  .then(stream => {
+    video.srcObject = stream;
+  })
+  .catch(error => {
+    console.error("Error accessing camera:", error);
+  });
+
+  window.addEventListener("load", () => {
+    // put the above code here
+  });
